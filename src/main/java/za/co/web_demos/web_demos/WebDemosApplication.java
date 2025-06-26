dece8b81a7c155b7dd6cc4d7e5d000667824db37
Nkosinathi Radebe <nkosinathi.khumalo@web-demos.co.za>
2025-06-26 22:41:14 +0200

    first commit

---
+++ src/main/java/za/co/web_demos/web_demos/WebDemosApplication.java
@@ -0,0 +1,13 @@
+package za.co.web_demos.web_demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/web_demos/web_demos/WebDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.web_demos.web_demos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
